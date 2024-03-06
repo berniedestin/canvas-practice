cd9d85a08cf10f3443838d50a33ab832378e6ff1
Destin <berniedestin7@gmail.com>
2024-03-05 20:28:20 -0500

    Add bouncy ball functionality

--- script.js
+++ script.js
@@ -59,14 +59,19 @@ window.addEventListener('mousemove', (event)=>{
 window.addEventListener('resize',()=>{
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    gravRender.width = window.innerWidth;
+    gravRender.height = window.innerHeight;
     init();
+    initBounce();
 });
 
 orbsGrowBtn.addEventListener('click',()=>{
     if(showOrbsGrow){
         canvas.style.display = 'none';
+        orbsGrowBtn.innerText = 'Show Orbs Grow on Hover';
     }else{
         canvas.style.display = 'block';
+        orbsGrowBtn.innerText = 'Hide Orbs Grow on Hover';
     }
     showOrbsGrow = !showOrbsGrow;
     modalTest.close();
@@ -127,7 +132,7 @@ class Circle{
 let circleArray = [];
 function init(){
     circleArray = [];
-    for(let i = 0; i <800;i++){
+    for(let i = 0; i <1000;i++){
         let rad = (Math.random()*4) +1;
         let x = (Math.random() * (window.innerWidth - (2 * rad))) + rad;
         let y = (Math.random() * (window.innerHeight - (2 * rad))) + rad;
@@ -147,19 +152,159 @@ function animateOrbsGrow(){
         circle.update();
     })
 
+    
+
 }
 
-animateOrbsGrow();
+//animateOrbsGrow();
 
-function main(){
-    if(showOrbsGrow){
-        animateOrbsGrow();
+// function main(){
+//     if(showOrbsGrow){
+//         animateOrbsGrow();
+//     }
+// }
+
+// main();
+// **********************************************************************************
+// gravity
+
+const gravRender = document.getElementById('gravRender');
+gravRender.width = window.innerWidth;
+gravRender.height = window.innerHeight;
+const gravContext = gravRender.getContext('2d');
+const gravBtn = document.getElementById('grav');
+let showBoucyBalls = false;
+const gravity = .9;
+gravBtn.addEventListener('click', ()=>{
+    if(showBoucyBalls){
+        gravRender.style.display = 'none';
+        gravBtn.innerText = 'Show Bouncy Balls';
+    }else{
+        gravRender.style.display = 'block';
+        gravBtn.innerText = 'Hide Bouncy Balls';
     }
-    main();
+    showBoucyBalls = !showBoucyBalls;
+    modalTest.close();
+
+});
+class Ball{
+    constructor(x,y,dx,dy,rad){
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.rad = rad;
+        
+        // pre-set properties
+        this.isDirectionChanged = false;
+        this.fillColor = colorPallet[Math.floor(Math.random()*colorPallet.length)];
+
+    }
+    draw(){
+        gravContext.beginPath();
+        gravContext.arc(this.x,this.y, this.rad, 0, Math.PI *2,true);
+        if(this.isDirectionChanged){
+            // some code here when direction changes
+            this.isDirectionChanged = false;
+        }
+        gravContext.fillStyle = this.fillColor;
+        gravContext.fill();
+    
+    }
+    update(){
+        if(this.x >= window.innerWidth - this.rad || this.x <= this.rad){
+            this.dx = this.dx * -1;
+            this.isDirectionChanged = true;
+        }
+        if(this.y >= window.innerHeight - this.rad - this.dy || this.y <= this.rad ){
+            this.dy = this.dy * -1 * .9;
+            this.isDirectionChanged = true;
+        }else {
+            this.dy += gravity;
+        }
+        if(this.dy > .01 || this.dy < -.01){
+            this.x += this.dx;
+        }
+        this.y += this.dy;
+        this.draw();
+
+    }
+
 }
+let clickLocation = {
+    x: window.innerWidth * .5,
+    y: window.innerHeight * .2
+}
+let ballArray = [];
+function initBounce(){
+    ballArray = [];
+    for(let i = 0; i < 50; i++){
+        let rad = (Math.random()*20) +10;
+        let x = clickLocation.x;
+        let y = clickLocation.y;     
+        let dx = Math.random() > 0.5 ? -1 - (Math.random() * 3): 1 + (Math.random() * 3);
+        let dy = Math.random() > 0.5 ? -1 - (Math.random() * 3): 1 + (Math.random() * 3);
+        ballArray.push(new Ball(x,y,dx,dy,rad));
+        
+    }
+
+}
+initBounce();
+
+window.addEventListener('click',(event)=>{
+    clickLocation.x = event.x;
+    clickLocation.y = event.y;
+    initBounce();
+
+})
+function animateBounce(){
+    requestAnimationFrame(animateBounce);
+    gravContext.clearRect(0,0,window.innerWidth,window.innerHeight);
+
+    ballArray.forEach((ball)=>{
+        ball.update();
+    })
+
+}
+//animateBounce();
 
 // **********************************************************************************
+function animate(){
+    requestAnimationFrame(animate);
+    if(showOrbsGrow){
+        context.clearRect(0,0,window.innerWidth,window.innerHeight);
+        //testCircle.update(); //this works
 
+        circleArray.forEach((circle)=>{
+            circle.update();
+        });
+    }
+    if(showBoucyBalls){
+        gravContext.clearRect(0,0,window.innerWidth,window.innerHeight);
+
+        ballArray.forEach((ball)=>{
+            ball.update();
+        });
+        // for(let i = 0; i < number; i++){
+        //     context.fillStyle = `rgba(${Math.random()*255},${Math.random()*255},${Math.random()*255},${Math.random()})`;
+        //     context.fillRect(Math.random() * window.innerWidth,Math.random() * window.innerHeight,Math.random() * size,Math.random() * size);
+        // }
+    
+    }
+    
+
+}
+
+animate();
+
+
+
+
+// **********************************************************************************
+// General animate??
+
+
+// **********************************************************************************
 function drawCircles(number, size){
 
     for(let i = 0; i < number; i++){
@@ -170,6 +315,7 @@ function drawCircles(number, size){
         context.fill();
         context.stroke();
     }
+    
 }
 
 
